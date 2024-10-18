385528e6e6526532e21e58b66b8029fae0fd3692
Lee MyoungHwan <amaramusic@gmail.com>
2024-10-18 21:13:24 +0900

    feat: update random selection for balance

--- api-server/config.py
+++ api-server/config.py
@@ -5,9 +5,11 @@ load_dotenv()
 
 SCHEDULER_INTERVAL = 60 * 5  # 5 minutes
 QUESTION_COUNT_BUFFER = 100
+
 OPENAI_API_VERSION = "2024-08-01-preview"
-AZURE_ENDPOINT = "https://cog-2zdwsmcbpa7va.openai.azure.com/"
+AZURE_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 USERNAME = "smartagent"
 DB_PASSWORD = os.getenv("DB_PASSWORD", "noPassword")
 CONNECT_STRING = f"postgresql+asyncpg://{USERNAME}:{DB_PASSWORD}@10.10.5.13:31655/smartagent"
