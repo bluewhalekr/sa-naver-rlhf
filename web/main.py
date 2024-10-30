886a000339a01032fc913079679e2dbe2e22e58c
mike <xcellentbird@gmail.com>
2024-10-30 11:33:48 +0900

    fix: login 기능 성능 개선

--- web/main.py
+++ web/main.py
@@ -18,26 +18,42 @@ ASSET_ROOT = os.path.join(WEB_ROOT, 'assets')
 ICON_PATH = os.path.join(ASSET_ROOT, "AIMMO-시그니처 로고_바이올렛+블랙.png")
 
 
-with open(AUTH_CONFIG_PATH, 'r') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+@st.cache_data
+def get_config():
+    with open(AUTH_CONFIG_PATH, 'r') as file:
+        return yaml.load(file, Loader=SafeLoader)
 
+
+config = get_config()
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
-)
+    )
 
 authenticator.login(location="unrendered")
+
+
+def login_view():
+    login_form = st.form(key="login_form")
+    login_form.subheader('Login')
+    username = login_form.text_input('Username')
+    password = login_form.text_input('Password', type='password')
+
+    if login_form.form_submit_button('Login'):
+        if authenticator.authentication_controller.login(username, password):
+            authenticator.cookie_controller.set_cookie()
+            logger.info(f"{st.session_state['username'].rjust(12)}| user authenticated")
 
 
 def login():
     st.logo(ICON_PATH, icon_image=ICON_PATH)
     if st.session_state['authentication_status'] is None:
-        authenticator.login()
+        login_view()
 
     elif not st.session_state['authentication_status']:
-        authenticator.login()
+        login_view()
         st.warning("wrong username or password")
 
 
@@ -56,8 +72,6 @@ q_gen_page = st.Page("q_gen_page.py", title="질문 생성", icon=":material/cha
 
 # Page routing
 if st.session_state['authentication_status']:
-    logger.info(f"{st.session_state['username'].rjust(12)}| user authenticated")
-
     page_dict = {"Question": [q_gen_page], "Account": [logout_page]}
     pg = st.navigation(page_dict)
 
