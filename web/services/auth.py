1de60b45ea505181876832f74e2e9fe600298706
mike <xcellentbird@gmail.com>
2025-02-05 03:45:46 +0900

    feat: 인증 서비스 코드

---
+++ web/services/auth.py
@@ -0,0 +1,30 @@
+from config import AUTH_API_URL
+from models import AuthRequestData, AuthResponse
+from services.module import ApiClient
+
+TEST_MODE = True
+MOCK_AUTH_RESPONSE = {
+    "status": "success",
+}
+
+
+def request_authenticate(username: str, token: str) -> AuthResponse:
+    if TEST_MODE:
+        json_response = MOCK_AUTH_RESPONSE
+
+    else:
+        api_client = ApiClient()
+
+        req_data = AuthRequestData(username=username, token=token)
+        json_response = api_client.post(AUTH_API_URL, data=req_data.dict())
+
+    return AuthResponse(**json_response)
+
+
+def authenticate(username, token) -> bool:
+    response = request_authenticate(username, token)
+
+    if response.status == "success":
+        return True
+
+    return False
